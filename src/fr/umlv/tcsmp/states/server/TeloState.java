7672c78a661dad22b4ec07e2d6a3d1b181c843f3
plouck <plouck@020f9b02-3690-11de-82fb-7f665ef830f7>
2009-05-06 12:10:54 +0000

    Some comments.

--- src/fr/umlv/tcsmp/states/server/TeloState.java
+++ src/fr/umlv/tcsmp/states/server/TeloState.java
@@ -8,6 +8,8 @@ import fr.umlv.tcsmp.states.TCSMPState;
 
 public class TeloState implements TCSMPState {
 
+	private static String banner = "Hello I'm the TCSMP server. Nice to meet you.";
+	
 	public Response processCommand(ByteBuffer bb) {
 		
 		String [] args = TCSMPCommandParser.parse(bb);
@@ -16,6 +18,18 @@ public class TeloState implements TCSMPState {
 			System.out.println("fook");
 			return null;
 		}
+
+		/**
+		 * XXX: Comment va-t-on faire pour changer de state ici ?
+		 * Faut passer le protocole en param ?
+		 */
+		
+		
+		/**
+		 * Ici on va créer un Response avec le code 200 et un welcome message.
+		 * XXX: On nique le bb qu'on a passé en param et on crée un Response qui
+		 * va bien ?
+		 */
 		
 		System.out.println("ok");
 		return new Response();
