ecc0c54a76ff4e6fe8409e40657c35fee4539039
plouck <plouck@020f9b02-3690-11de-82fb-7f665ef830f7>
2009-05-06 13:03:35 +0000

    Update !

--- src/fr/umlv/tcsmp/states/server/TeloState.java
+++ src/fr/umlv/tcsmp/states/server/TeloState.java
@@ -6,30 +6,22 @@ import fr.umlv.tcsmp.proto.Protocol;
 import fr.umlv.tcsmp.proto.Response;
 import fr.umlv.tcsmp.proto.TCSMPCommandParser;
 import fr.umlv.tcsmp.states.TCSMPState;
+import fr.umlv.tcsmp.utils.ErrorReply;
 
 public class TeloState implements TCSMPState {
 
-	private static String banner = "Hello I'm the TCSMP server. Nice to meet you.";
 	
 	public Response processCommand(Protocol proto, ByteBuffer bb) {
 		
 		String [] args = TCSMPCommandParser.parse(bb);
 		
-		if (args.length == 0 || args[0].equals("TELO") == false) {
-			System.out.println("fook");
-			return null;
+		if (args.length != 2 || args[0].equals("TELO") == false) {
+			return new Response(ErrorReply.unknowCommand("TELO", args[0]));
 		}
 
 		/**
 		 * XXX: Comment va-t-on faire pour changer de state ici ?
-		 * Faut passer le protocole en param ?
-		 */
-		
-		
-		/**
-		 * Ici on va créer un Response avec le code 200 et un welcome message.
-		 * XXX: On nique le bb qu'on a passé en param et on crée un Response qui
-		 * va bien ?
+		 * Faut passer le protocole en param ? (Ce que je fais là)
 		 */
 		
 		/**
@@ -40,9 +32,8 @@ public class TeloState implements TCSMPState {
 		/**
 		 * Create response buffer.
 		 */
-		ByteBuffer response = ByteBuffer.wrap(new String("200-TCSMPv1\r\n200 OK " + banner + "\r\n").getBytes());
+		ByteBuffer response = ByteBuffer.wrap(new String("200 TCSMPv1\r\n200-OK greets " + args[1] + "\r\n").getBytes());
 		
-		System.out.println("ok");
 		return new Response(response);
 	}
 }
