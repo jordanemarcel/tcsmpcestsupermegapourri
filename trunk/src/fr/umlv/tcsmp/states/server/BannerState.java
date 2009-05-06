24e72037d2047080248472acd605eb28927b2412
plouck <plouck@020f9b02-3690-11de-82fb-7f665ef830f7>
2009-05-06 13:18:27 +0000

    Add new state.

--- trunk/src/fr/umlv/tcsmp/states/server/BannerState.java
+++ trunk/src/fr/umlv/tcsmp/states/server/BannerState.java
@@ -8,7 +8,7 @@ import fr.umlv.tcsmp.states.TCSMPState;
 
 public class BannerState implements TCSMPState {
 
-	private static String banner = "200 Hello I'm the TCSMP server. Nice to meet you.";
+	private static String banner = "200 Hello I'm the TCSMP server. Nice to meet you.\r\n";
 
 	@Override
 	public Response processCommand(Protocol proto, ByteBuffer bb) {
