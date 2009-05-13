5b4980cae5eed0907bce4889e9491470edb3c9c4
jordane.marcel <jordane.marcel@020f9b02-3690-11de-82fb-7f665ef830f7>
2009-05-13 21:24:23 +0000

    Commentaries added. Following up: better exception handlers

--- trunk/src/fr/umlv/tcsmp/tcp/KeyAttachment.java
+++ trunk/src/fr/umlv/tcsmp/tcp/KeyAttachment.java
@@ -5,34 +5,66 @@ import java.nio.ByteBuffer;
 import fr.umlv.tcsmp.proto.Protocol;
 import fr.umlv.tcsmp.proto.Response;
 
+/**
+ * This class represents the object that is attached to a selector key.
+ * It contains necessary information for the current selected TCP channel.
+ */
 public class KeyAttachment {
+	/** The ByteBuffer associated with a SocketChannel */
 	private final ByteBuffer byteBuffer;
+	/** The protocol associated with a SocketChannel, one per session */
 	private final Protocol protocol;
+	/** The last response that was received */
 	private Response currentResponse;
 	
+	/**
+	 * Creates the KeyAttachment object with the given ByteBuffer and Protocol.
+	 * @param byteBuffer - the given ByteBuffer
+	 * @param protocol - the given Protocol
+	 */
 	public KeyAttachment(ByteBuffer byteBuffer, Protocol protocol) {
 		this.byteBuffer = byteBuffer;
 		this.protocol = protocol;
 	}
 	
+	/**
+	 * Creates a copy of the given KeyAttachement, only duplicating its ByteBuffer.
+	 * @param keyAttachment - the KeyAttachement to copy
+	 */
 	public KeyAttachment(KeyAttachment keyAttachment) {
 		this.byteBuffer = keyAttachment.getByteBuffer().duplicate();
 		this.protocol = keyAttachment.getProtocol();
 		this.currentResponse = keyAttachment.currentResponse;
 	}
 	
+	/**
+	 * Returns the ByteBuffer
+	 * @return - the ByteBuffer
+	 */
 	public ByteBuffer getByteBuffer() {
 		return byteBuffer;
 	}
 	
+	/**
+	 * Returns the Protocol
+	 * @return - the Protocol
+	 */
 	public Protocol getProtocol() {
 		return protocol;
 	}
 	
+	/**
+	 * Sets the last Response
+	 * @param currentResponse - the last Response
+	 */
 	public void setCurrentResponse(Response currentResponse) {
 		this.currentResponse = currentResponse;
 	}
 	
+	/**
+	 * Returns the last Response
+	 * @return - the last Response
+	 */
 	public Response getCurrentResponse() {
 		return currentResponse;
 	}
--- trunk/src/fr/umlv/tcsmp/tcp/SocketData.java
+++ trunk/src/fr/umlv/tcsmp/tcp/SocketData.java
@@ -4,22 +4,45 @@ import java.nio.channels.SocketChannel;
 import java.util.Collection;
 import java.util.HashMap;
 
+/**
+ * This class represents a list of clients of a protocol session, including the
+ * original client that initialized the session.
+ */
 public class SocketData {
+	/** The client that initialized the session */
 	private final SocketChannel originalClient;
+	/** A map of sockets and their corresponding domain */
 	private final HashMap<String, SocketChannel> domainSocketMap = new HashMap<String, SocketChannel>();
 	
+	/**
+	 * Constructs a new SocketData and sets the original client
+	 * @param originalClient - the original client
+	 */
 	public SocketData(SocketChannel originalClient) {
 		this.originalClient = originalClient;
 	}
 	
+	/**
+	 * Returns the original client of the session
+	 * @return - the original client
+	 */
 	public SocketChannel getOriginalClient() {
 		return originalClient;
 	}
 	
+	/**
+	 * Returns the socket that correspond with the given domain
+	 * @param domain - the given domain
+	 * @return - The SocketChannel that correspond with the given domain, null otherwise.
+	 */
 	public SocketChannel getSocket(String domain) {
 		return domainSocketMap.get(domain);
 	}
 	
+	/**
+	 * Return the Collection of connected clients
+	 * @return - the connection of connected clients
+	 */
 	public Collection<SocketChannel> getClients() {
 		return domainSocketMap.values();
 	}
