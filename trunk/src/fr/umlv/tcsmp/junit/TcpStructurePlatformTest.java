d111445800d28b304044ff23057559cba808d928
jordane.marcel <jordane.marcel@020f9b02-3690-11de-82fb-7f665ef830f7>
2009-05-13 19:22:18 +0000

    Added the client mode in the TcpStructure.

---
+++ trunk/src/fr/umlv/tcsmp/junit/TcpStructurePlatformTest.java
@@ -0,0 +1,13 @@
+package fr.umlv.tcsmp.junit;
+
+import fr.umlv.tcsmp.dns.TCSMPResolver;
+import fr.umlv.tcsmp.tcp.TcpStructure;
+
+public class TcpStructurePlatformTest {
+
+	public static void main(String[] args) {
+		TCSMPResolver resolver = new TCSMPResolver();
+		TcpStructure tcpStructure = new TcpStructure()
+	}
+	
+}
