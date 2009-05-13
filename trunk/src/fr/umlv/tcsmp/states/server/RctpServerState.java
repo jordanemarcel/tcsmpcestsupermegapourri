490f19b55a610c3559730b60335690033b765114
plouck <plouck@020f9b02-3690-11de-82fb-7f665ef830f7>
2009-05-13 23:58:08 +0000

    Relay seems to work ATM.

--- trunk/src/fr/umlv/tcsmp/states/server/RctpServerState.java
+++ trunk/src/fr/umlv/tcsmp/states/server/RctpServerState.java
@@ -15,63 +15,74 @@ import fr.umlv.tcsmp.utils.TCSMPParser;
 public class RctpServerState extends TCSMPState {
 
 	private boolean send = false;
-	
+
 	private String currentRCPTDomain;
+	private String serverResponse;
 	private Protocol fakeProto;
-	
+
 	@Override
 	public Response processCommand(Protocol proto, ByteBuffer bb) {
-		
+
 		/** we are in a relaying mode
 		 */
 		if (fakeProto != null) {
+
+			// save each time the response of the server
+			serverResponse = TCSMPParser.decode(bb);
 			
 			// get response from the fake proto.
 			Response res = fakeProto.doIt(bb);
-		
+
 			// reply code to the client
-			if (res.getAction() == ResponseAction.READ && send == false) {
-				fakeProto = null;
-				bb.position(0);
-				return new Response(ResponseAction.WRITE);
+			if (send == false) {
+				if (res.getAction() == ResponseAction.WRITE) {
+					fakeProto = null;
+					bb.clear();
+					bb.put(TCSMPParser.encode(serverResponse));
+					bb.flip();
+					return new Response(ResponseAction.WRITE);
+				}
+				else {
+					return new Response(currentRCPTDomain, ResponseAction.READ);
+				}
 			}
 
 			// last state, the next response will be replied to the client
 			if (fakeProto.getState().getClass().equals(RctpClientState.class)) {
 				send = false;
 			}
-			
+
 			// override response in order to add the currentRCPTDomain
-			if (res.getAction() != ResponseAction.READ) {	
+			if (res.getAction() != ResponseAction.READ) {
 				return new Response(currentRCPTDomain, ResponseAction.WRITE);
 			}
-			
-			bb.clear();
+
 			return new Response(currentRCPTDomain, ResponseAction.READ);
 		}
-		
+
 		if (send) {
 			send = false;
+			bb.clear();
 			return new Response(ResponseAction.READ);
 		}
-		
+
 		send = true;
-		
+
 		String [] args = TCSMPParser.parseCommand(bb);
 		bb.clear();
-		
+
 		if (args.length == 1 && args[0].equals("QUIT")) {
 			TCSMPState t = new QuitServerState();
 			proto.setState(t);
 			return t.processCommand(proto, bb);
 		}
-		
+
 		if (args.length > 2 || (args[0].equals("RCPT") == false && args[0].equals("APZL") == false)) {
 			bb.put(ErrorReplies.unknowCommand("RCPT|APZL", args[0]));
 			bb.flip();
 			return new Response(ResponseAction.WRITE);
 		}
-		
+
 		// manual switch state if needed
 		if (args[0].equals("APZL")) {
 			TCSMPState apzlState = new ApzlServerState();
@@ -81,8 +92,8 @@ public class RctpServerState extends TCSMPState {
 			bb.flip();
 			return apzlState.processCommand(proto, bb);
 		}
-		
-		
+
+
 		// Check address and add it to the rctps array
 		String domain;
 		try {
@@ -94,14 +105,14 @@ public class RctpServerState extends TCSMPState {
 			bb.flip();
 			return new Response(ResponseAction.WRITE);
 		}
-		
+
 		if (proto.isRelay(domain) == false) {
 			bb.put(TCSMPParser.encode("250 OK\r\n"));
 			bb.flip();
 			return new Response(ResponseAction.WRITE);
 		}
 
-		
+
 		// Create a fakeProto for our client states
 		fakeProto = proto.newProtocol();
 		fakeProto.setState(new TeloClientState());
@@ -109,8 +120,7 @@ public class RctpServerState extends TCSMPState {
 		Response res = fakeProto.doIt(bb);
 		if (res.getAction() != ResponseAction.READ)
 			return new Response(currentRCPTDomain, ResponseAction.WRITE);
-		
-		bb.clear();
+
 		return new Response(currentRCPTDomain, ResponseAction.READ);
 	}
 }
