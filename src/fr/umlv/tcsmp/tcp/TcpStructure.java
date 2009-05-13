a4c42984a7e00f0da7686ec79127a5fdaa163dc2
jordane.marcel <jordane.marcel@020f9b02-3690-11de-82fb-7f665ef830f7>
2009-05-13 21:24:23 +0000

    Commentaries added. Following up: better exception handlers

--- src/fr/umlv/tcsmp/tcp/TcpStructure.java
+++ src/fr/umlv/tcsmp/tcp/TcpStructure.java
@@ -16,27 +16,46 @@ import java.util.Set;
 
 import fr.umlv.tcsmp.dns.TCSMPResolver;
 import fr.umlv.tcsmp.proto.Protocol;
+import fr.umlv.tcsmp.proto.ProtocolMode;
 import fr.umlv.tcsmp.proto.Response;
 import fr.umlv.tcsmp.proto.ResponseAction;
 
+/**
+ * This class is a generic TCP Client/Server. It is given a Protocol object which determines
+ * if the TCP structure behaves like a client or server. It uses a Selector.
+ * Every time the TCP session reads or writes on the network, it then asks to the Protocol object
+ * what its next action should be.
+ */
 public class TcpStructure {
+	/** Default TCP Application layer buffer size */
 	public static int BUFFER_SIZE = 1024;
-
+	/** Selector of the TCP structure */
 	private final Selector selector;
+	/** Dns Resolver of the TCP structure */
 	private final TCSMPResolver dnsResolver;
+	/** Protocol given to the TCP structure */
 	private Protocol givenProtocol;
+	/** Map that associates a Protocol (session) with a SocketData (list of connected
+	 *  clients) */
 	private final HashMap<Protocol, SocketData> protocolDomainMap = 
 		new HashMap<Protocol, SocketData>();
 
-	public TcpStructure() throws IOException {
-		this(new TCSMPResolver());
-	}
-
+	/**
+	 * Builds a new TCP structure with a given TCSMP Resolver
+	 * @param dnsResolver - A DNS Resolver
+	 * @throws IOException - if the selector can't be open
+	 */
 	public TcpStructure(TCSMPResolver dnsResolver) throws IOException {
 		this.selector = Selector.open();
 		this.dnsResolver = dnsResolver;
 	}
 
+	/**
+	 * Launches the TCP structure with the given protocol. The protocol should indicates
+	 * if it acts as a client or as a server.
+	 * @param protocol - the protocol that handles the Application layer of the TCP Structure
+	 * @throws IOException - if something goes wrong
+	 */
 	public void processProtocol(Protocol protocol) throws IOException {
 		this.givenProtocol = protocol;
 		switch (protocol.getProtocolMode()) {
@@ -51,6 +70,11 @@ public class TcpStructure {
 		}
 	}
 	
+	/**
+	 * Starts the structure in Client mode.
+	 * @param port - port the client is going to connect on
+	 * @throws IOException - if the connection goes wrong
+	 */
 	private void startClient(int port) throws IOException {
 		ByteBuffer byteBuffer = ByteBuffer.allocateDirect(TcpStructure.BUFFER_SIZE);
 		KeyAttachment keyAttachment = new KeyAttachment(byteBuffer, givenProtocol);
@@ -62,6 +86,11 @@ public class TcpStructure {
 		System.out.println("* TcpStructure: End of Transmission");
 	}
 
+	/**
+	 * Starts the structure in Server mode.
+	 * @param port - port that the server opens
+	 * @throws IOException - if the server goes wrong
+	 */
 	private void startServer(int port) throws IOException {
 		ServerSocketChannel serverSocketChannel = ServerSocketChannel.open();
 		InetSocketAddress localIsa = new InetSocketAddress(port);
@@ -71,9 +100,14 @@ public class TcpStructure {
 		this.handleSelector();
 	}
 	
+	/**
+	 * Starts the selector. If the TCP Structure is in Client mode, stops when
+	 * no more socket is connected. In Server mode, loop infinitely.
+	 * @throws IOException - if the selector goes wrong
+	 */
 	private void handleSelector() throws IOException {
 		int nbKeysSelected;
-		while(true) {
+		while(selector.keys().size()>0 || givenProtocol.getProtocolMode()==ProtocolMode.SERVER) {
 			do {
 				nbKeysSelected = selector.select();
 			} while(nbKeysSelected<1);
@@ -98,6 +132,13 @@ public class TcpStructure {
 		}
 	}
 
+	/**
+	 * Response handler. Each time the TCP structure writes or reads on the network, it asks
+	 * thereafter the protocol what it is going to do. The answer impacts on the selector
+	 * register system, the creation or closure of TCP connection.
+	 * @param key - The selected key associated with the response
+	 * @param response - The response given by the protocol
+	 */
 	private void handleResponse(SelectionKey key, Response response) {
 		SocketChannel socketChannel = (SocketChannel)key.channel();
 		KeyAttachment keyAttachment = (KeyAttachment)key.attachment();
@@ -153,14 +194,28 @@ public class TcpStructure {
 				return;
 			}
 		} catch (ClosedChannelException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			key.cancel();
+			System.err.println(e);
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			if(!socketChannel.isConnected()) {
+				try {
+					socketChannel.close();
+				} catch (IOException e1) {
+					System.err.println(e);
+				}
+			}
+			key.cancel();
+			System.err.println(e);
 		}
 	}
 
+	/**
+	 * First, this method resolves the domain using the DNS Resolver and gets an address
+	 * of a server. Then, it connects this new server and register correctly with the selector.
+	 * @param domain - the domain that is going to be resolved to get the address of a server
+	 * @param keyAttachment - The keyAttachment that contains information about the session protocol,
+	 * the ByteBuffer and the last response given by the protocol.
+	 */
 	private void connectNewClient(String domain, KeyAttachment keyAttachment) {
 		if(domain==null) {
 			throw new IllegalArgumentException("Can't establish a connection with a null client :-(");
@@ -178,17 +233,18 @@ public class TcpStructure {
 				client.register(selector, SelectionKey.OP_CONNECT, keyAttachment);
 			}
 		} catch(IllegalArgumentException iae) {
-			// TODO Auto-generated catch block
-			iae.printStackTrace();
+			System.err.println(iae);
 		} catch (UnknownHostException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			System.err.println(e);
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			System.err.println(e);
 		}
 	}
 
+	/**
+	 * Selector method: accepts a new connection
+	 * @param key - selected key
+	 */
 	private void doAccept(SelectionKey key) {
 		ServerSocketChannel serverSocketChannel = (ServerSocketChannel)key.channel();
 		try {
@@ -206,7 +262,7 @@ public class TcpStructure {
 			switch (responseAction) {
 			case READ: case WRITE: 
 				socketChannel.register(selector, TcpStructure.getResponseOps(responseAction), keyAttachment);
-				break;
+				return;
 			case CLOSE:
 				socketChannel.close();
 				return;
@@ -218,6 +274,10 @@ public class TcpStructure {
 		}
 	}
 
+	/**
+	 * Selector method: read from a socket
+	 * @param key - selected key
+	 */
 	private void doRead(SelectionKey key) {
 		SocketChannel socketChannel = (SocketChannel)key.channel();
 		KeyAttachment keyAttachment = (KeyAttachment)key.attachment();
@@ -226,14 +286,26 @@ public class TcpStructure {
 		System.out.println("* TcpStructure: Reading from " + socketChannel.socket().getRemoteSocketAddress());
 		try {
 			socketChannel.read(byteBuffer);
+			System.out.println("* TcpStructure: Buffer state: " + byteBuffer);
 			Response response = protocol.doIt(byteBuffer);
 			this.handleResponse(key, response);
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			System.err.println(e);
+			if(!socketChannel.isConnected()) {
+				try {
+					socketChannel.close();
+				} catch (IOException e1) {
+					System.err.println(e1);
+				}
+			}
+			key.cancel();
 		}
 	}
 
+	/**
+	 * Selector method: writes to a socket
+	 * @param key - selected key
+	 */
 	private void doWrite(SelectionKey key) {
 		SocketChannel socketChannel = (SocketChannel)key.channel();
 		KeyAttachment keyAttachment = (KeyAttachment)key.attachment();
@@ -245,14 +317,25 @@ public class TcpStructure {
 				socketChannel.write(byteBuffer);
 			}
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			System.err.println(e);
+			if(!socketChannel.isConnected()) {
+				try {
+					socketChannel.close();
+				} catch (IOException e1) {
+					System.err.println(e1);
+				}
+			}
+			key.cancel();
 		}
 		byteBuffer.clear();
 		Response response = protocol.doIt(byteBuffer);
 		this.handleResponse(key, response);
 	}
 
+	/**
+	 * Selector method: connects to a socket
+	 * @param key - selected key
+	 */
 	private void doConnect(SelectionKey key) {
 		SocketChannel socketChannel = (SocketChannel)key.channel();
 		KeyAttachment keyAttachment = (KeyAttachment)key.attachment();
@@ -263,11 +346,24 @@ public class TcpStructure {
 			}
 			key.interestOps(TcpStructure.getResponseOps(keyAttachment.getCurrentResponse().getAction()));
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			System.out.println(e);
+			if(!socketChannel.isConnected()) {
+				try {
+					socketChannel.close();
+				} catch (IOException e1) {
+					System.err.println(e1);
+				}
+			}
+			key.cancel();
 		}
 	}
 
+	/**
+	 * Static method that returns the interesting Operation for a SocketChannel that wants to register
+	 *  with a selector, corresponding to the given ResponseAction
+	 * @param responseAction - the given ResponseAction
+	 * @return - The interesting Operation for the socket that wants to register with the selector
+	 */
 	private static int getResponseOps(ResponseAction responseAction) {
 		switch (responseAction) {
 		case READ:
