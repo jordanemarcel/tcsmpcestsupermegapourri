a2172a418a3a55f33f5d6617357ebb5802abd768
plouck <plouck@020f9b02-3690-11de-82fb-7f665ef830f7>
2009-05-14 09:13:13 +0000

    Simple relaying is now working.

--- trunk/src/fr/umlv/tcsmp/states/server/QuitServerState.java
+++ trunk/src/fr/umlv/tcsmp/states/server/QuitServerState.java
@@ -1,6 +1,8 @@
 package fr.umlv.tcsmp.states.server;
 
 import java.nio.ByteBuffer;
+import java.text.ParseException;
+import java.util.LinkedList;
 
 import fr.umlv.tcsmp.proto.Protocol;
 import fr.umlv.tcsmp.proto.Response;
@@ -13,7 +15,23 @@ public class QuitServerState extends TCSMPState {
 	
 	private boolean send = false;
 	
+	// domains not treated
+	private LinkedList<String> domains;
+	
 	public Response processCommand(Protocol proto, ByteBuffer bb) {
+		
+		// we are in a relaying mode
+		if (domains != null) {
+			
+			// finished reply the last QUIT cmd to the client
+			if (domains.size() == 0) {
+				bb.position(0);
+				return new Response(ResponseAction.WRITE);
+			}
+			
+			// read the next domain
+			return new Response(domains.removeFirst(), ResponseAction.READ);
+		}
 		
 		if (send) {
 			bb.clear();
@@ -22,9 +40,9 @@ public class QuitServerState extends TCSMPState {
 		
 		send = true;
 		String [] args = TCSMPParser.parseCommand(bb);
-		bb.clear();
 		
 		if (args[0].equals("QUIT") == false) {
+			bb.clear();
 			bb.put(ErrorReplies.unknowCommand("QUIT", args[0]));
 			bb.flip();
 			return new Response(ResponseAction.WRITE);
@@ -32,12 +50,24 @@ public class QuitServerState extends TCSMPState {
 		
 		// Check if we have to forward the command.
 		if (proto.isRelay() == false) {
+			bb.clear();
 			bb.put(TCSMPParser.encode("250 See you next time~~~~!\r\n"));
 			bb.flip();
 			return new Response(ResponseAction.WRITE);
 		}
 		
 		// Reset command and forward
+		bb.position(0);
+		
+		// create a list with all the domain of the rctp
+		domains = new LinkedList<String>();
+		for (String r : proto.getRecpts()) {
+			try {
+				domains.add(TCSMPParser.parseDomain(r));
+			} catch (ParseException e) {
+			}
+		}
+
 		return new Response(ResponseAction.RELAYALL);
 	}
 	
