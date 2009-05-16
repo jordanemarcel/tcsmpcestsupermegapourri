0cfc9718479ae198e07ecc88c302e4997b2574f4
plouck <plouck@020f9b02-3690-11de-82fb-7f665ef830f7>
2009-05-16 15:08:21 +0000

    look for domain in a file.

--- src/fr/umlv/tcsmp/junit/TcpStructurePlatformTest.java
+++ src/fr/umlv/tcsmp/junit/TcpStructurePlatformTest.java
@@ -13,11 +13,11 @@ import fr.umlv.tcsmp.tcp.TcpStructure;
 public class TcpStructurePlatformTest {
 
 	public static void main(String[] args) {
-		DNSResolver resolver = new TCSMPResolver();
+		DNSResolver resolver = new TCSMPResolver("127.0.0.1", "/tmp/domains");
 		try {
 			TcpStructure tcpStructure = new TcpStructure(resolver);
 			Protocol protocol = new Protocol(ProtocolMode.SERVER, 26);
-			if(args.length==1) {
+			if(args.length == 1) {
 				protocol.addDomain(args[0]);
 			} else {
 				protocol.addDomain("foobar.com");
