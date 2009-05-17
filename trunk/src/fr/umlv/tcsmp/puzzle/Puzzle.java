dda7e9f9d04fa87c75773d9fa66c5142ca118233
Lebreton.Clement <Lebreton.Clement@020f9b02-3690-11de-82fb-7f665ef830f7>
2009-05-17 11:16:25 +0000

    Javadoc et quelque commentaire

--- trunk/src/fr/umlv/tcsmp/puzzle/Puzzle.java
+++ trunk/src/fr/umlv/tcsmp/puzzle/Puzzle.java
@@ -8,69 +8,105 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Random;
 
-
+/**
+ * This class is an implementation of a eternity puzzle. With this class you can
+ * generate puzzle for a string, or generate random puzzle with specified number of wheel
+ * (in width and in height).
+ * This class provide also a solver which can resolved eternity puzzle.
+ * 
+ * @author Clément Lebreton & Jordane Marcel & Rémy Masson & Clément Lecigne
+ */
 public class Puzzle {
-	public static void main(String[] args) {
-		Puzzle p = Puzzle.randomPuzzle(6, 6);
-		Puzzle p2 = new Puzzle(6, 6, p.lineString());
-		shuffle(p);
-		System.out.println(p);
-		System.out.println(p.equals(p2));
-		resolve(p);
-
-		
-		
-		// My test
-		Puzzle puz = new Puzzle(4, 4, "ntct41thdjhmiomapeo7157ojqolocl866vr5triq0iuceut67chtphk0fk2e92f");
-//		resolve(puz);
-		System.out.println("ntct41thdjhmiom6t6v7157ojqolocl866vr5triq0iuceut67chtphk0fk2e92f");
-		System.out.println("vs");
-		System.out.println(puz.lineString());
-	}
+	/**
+	 * This inner class provide a implementation of a Wheel of an eternity puzzle
+	 * This wheel provide some basic operation like rotate, un-rotate. 
+	 * @author Clément Lebreton & Jordane Marcel & Rémy Masson & Clément Lecigne
+	 */
 	static class Wheel{
-		private LinkedList<Character> ll = new LinkedList<Character>();
+		private ArrayList<Character> ll = new ArrayList<Character>();
 		private int offset=0;
+		/**
+		 * Constructs a new Wheel with the given String. The first letter is at the
+		 * north, the second at the east, the third at the south, and the fourth at
+		 * the west. 
+		 * 
+		 * @param values the given string. it have to contains 4 char only.
+		 */
 		public Wheel(String values) {
-			if(values.length()!=4)
+			if(values==null || values.length()!=4)
 				throw new IllegalArgumentException("Should contains 4 char");
 			for(Character c : values.toCharArray()){
 				ll.add(c);
 			}
 		}
-		public void rotate(){
+		/**
+		 * Rotates the wheel in the anticlockwise. e.g. : north become west
+		 */
+		public void unrotate(){
 			offset++;
 			if(offset>3){
 				offset=0;
 			}
 		}
-		public void unrotate(){
+		/**
+		 * Rotates the wheel in the clockwise. e.g. : north become east
+		 */
+		public void rotate(){
 			offset--;
 			if(offset<0){
 				offset=3;
 			}
 		}
+		/**
+		 * Returns the number of quarter of turn done.
+		 * @return the number of quarter of turn done.Always between 0 and 3 include
+		 */
 		public int getOffset() {
 			return offset;
 		}
+		/**
+		 * Set the number of rotate to do.
+		 * @param offset the number of rotate. Have to be between 0 and 3 include
+		 */
 		public void setOffset(int offset) {
 			if(offset>3){
 				throw new IllegalArgumentException("Offset should not be greater than 3");
 			}
+			if(offset<0)
+				throw new IllegalArgumentException("Offset have to be positive");
 			this.offset = offset;
 		}
+		/**
+		 * Returns the value read on the north
+		 * @return the value read on the north
+		 */
 		public char readNorth(){
 			return ll.get(offset);
 		}
+		/**
+		 * Returns the value read on the east
+		 * @return the value read on the east
+		 */
 		public char readEast(){
 			return ll.get((offset+1)%4);
 		}
+		/**
+		 * Returns the value read on the south
+		 * @return the value read on the south
+		 */
 		public char readSouth(){
 			return ll.get((offset+2)%4);
 		}
+		/**
+		 * Returns the value read on the west
+		 * @return the value read on the west
+		 */
 		public char readWest(){
 			return ll.get((offset+3)%4);
 		}
 		private void setNorth(Character c){
+			if(c==null)
+				throw new IllegalArgumentException("c should not be null");
 			ll.set(offset,c);
 		}
 		private void setEast(Character c){
@@ -82,7 +118,11 @@ public class Puzzle {
 		private void setWest(Character c){
 			ll.set((offset+3)%4,c);
 		}
-
+		/**
+		 * Returns if the wheel is equals to the given object. Two wheels are equals
+		 * if they have the same sequence of char read in the clockwise. e.g. : abcd and dabc is equals
+		 * @return true if obj and the wheels are equals, false otherwise
+		 */
 		@Override
 		public boolean equals(Object obj) {
 			if (! (obj instanceof Wheel) )
@@ -116,23 +156,14 @@ public class Puzzle {
 				this.rotate();
 			}
 			
-//			for(int i=0;i<4 && w.readSouth() != this.readSouth();i++){
-//				this.rotate();
-//			}
-//			boolean eq=true;
-//			for(int i=0; i<4;i++){
-//				w.rotate();
-//				this.rotate();
-//				if(w.readSouth() != this.readSouth()){
-//					eq=false;
-//					break;
-//				}
-//			}
 			
 			this.setOffset(bak);
 			w.setOffset(obak);
 			return false;
 		}
+		/**
+		 * Returns a string that represent a wheel.
+		 */
 		@Override
 		public String toString() {
 			StringBuilder sb = new StringBuilder();
@@ -143,25 +174,55 @@ public class Puzzle {
 			return sb.toString();
 		}
 	}
-	private LinkedList<Wheel> wheels = new LinkedList<Wheel>();
+	private ArrayList<Wheel> wheels = new ArrayList<Wheel>();
 	private int width;
 	private int height;
+	/**
+	 * Constructs a new Puzzle with the given width and height. Its wheels are build with values.
+	 * @param width the number of wheel in width
+	 * @param height the number of wheel in height
+	 * @param values the char use to build all wheels. values have to contains width*height*4 char.
+	 */
 	public Puzzle(int width,int height,String values) {
+		if(width<=0 || height<=0)
+			throw new IllegalArgumentException("width and height have to be positive");
 		this.width = width;
 		this.height = height;
-		if(values.length() != width*height*4)
+		if(values==null || values.length() != width*height*4)
 			throw new IllegalArgumentException("Incorrect char number");
 		for(int i = 0;i<width*height;i++){		
 			CharSequence wheelVal = values.subSequence(i*4, i*4+4);
 			wheels.add(new Wheel(wheelVal.toString()));
 		}
 	}
+	/**
+	 * Constructs a new Puzzle with the given width and height. Its wheels are in the given.
+	 * @param width the number of wheel in width
+	 * @param height the number of wheel in height
+	 * @param wheelList the list of wheel use to build the puzzle. Have to contains
+	 * width*height wheels
+	 */
 	public Puzzle(int width,int height,List<Wheel> wheelList){
+		if(width<=0 || height<=0)
+			throw new IllegalArgumentException("width and height have to be positive");
+		if(wheels==null || wheelList.size()!=width*height)
+			throw new IllegalArgumentException("wheels have to contains heigth*width wheels");
 		wheels.addAll(wheelList);
 		this.width = width;
 		this.height = height;
 	}
+	/**
+	 * Constructs a new Puzzle with the given width and height. Its wheels are in the given.
+	 * @param width the number of wheel in width
+	 * @param height the number of wheel in height
+	 * @param wheelList the list of wheel use to build the puzzle. Have to contains
+	 * width*height wheels
+	 */
 	public Puzzle(int width,int height,Wheel[][] wheels){
+		if(width<=0 || height<=0)
+			throw new IllegalArgumentException("width and height have to be positive");
+		if(wheels==null || wheels.length!=width || wheels[0].length!=height)
+			throw new IllegalArgumentException("wheels have to contains heigth*width wheels");
 		for(int j = 0;j<height;j++){
 			for(int i = 0;i<width;i++){
 				this.wheels.add(wheels[i][j]);
@@ -170,22 +231,46 @@ public class Puzzle {
 		this.width = width;
 		this.height = height;
 	}
-
+	/**
+	 * Returns the number of wheel in height
+	 * @return the number of wheel in height
+	 */
 	public int getHeight() {
 		return height;
 	}
+	/**
+	 * Returns the number of wheel in width
+	 * @return the number of wheel in width
+	 */
 	public int getWidth() {
 		return width;
 	}
+	/**
+	 * Returns the list of wheel of the puzzle. WARNING this list can be modify but you should
+	 * not do that.
+	 * @return
+	 */
 	public List<Wheel> getWheels() {
 		return wheels;
 	}
-	public void setWheels(LinkedList<Wheel> wheels) {
+	/**
+	 * Changes the wheels of this puzzle
+	 * @param wheels
+	 */
+	private void setWheels(List<Wheel> wheels) {
 		if(this.wheels.size()!=wheels.size())
 			return;
-		this.wheels = wheels;
+		this.wheels.clear();
+		this.wheels.addAll(wheels);
 	}
+	/**
+	 * Shuffle a puzzle. Each wheels is rotate in random position, and 
+	 * all wheels in the puzzle are shuffle too.
+	 * @param puzzle the puzzle to shuffle.
+	 */
 	public static void shuffle(Puzzle puzzle){
+		if(puzzle==null)
+			throw new IllegalArgumentException("puzzle should not be null");
 		List<Wheel> wheels = puzzle.getWheels();
 		Random rand = new Random();
 		for (Wheel w : wheels){
@@ -195,9 +280,19 @@ public class Puzzle {
 		}
 		Collections.shuffle(wheels);
 	}
+	/**
+	 * Generates a random puzzle of the given width and height. WARNING this puzzle is return SOLVED
+	 * you have to shuffle it if you want a challenge 
+	 * @param width the number of wheel in width that is queried
+	 * @param height the number of wheel in height that is queried
+	 * @return the generated puzzle. This puzzle is already solved! 
+	 */
 	public static Puzzle randomPuzzle(int width,int height){
+		if(width<=0 || height<=0)
+			throw new IllegalArgumentException("width and height have to be positive");
 		Wheel puzzle[][] = new Wheel[width][height];
 		Random rand = new Random();
+		//create random wheels
 		for(int i = 0;i<width;i++){
 			for(int j = 0;j<height;j++){
 				puzzle[i][j]=new Wheel("aaaa");
@@ -209,6 +304,7 @@ public class Puzzle {
 
 			}
 		}
+		//change wheel for make a resolvable puzzle
 		for(int i = 0;i<width;i++){
 			for(int j = 0;j<height;j++){
 				if(j+1<height && puzzle[i][j].readSouth() != puzzle[i][j+1].readNorth()){					
@@ -241,9 +337,8 @@ public class Puzzle {
 			for(int k = 0;k<3;k++){
 				sb[k] = new StringBuilder();
 			}
-
 			for(int j = 0;j<width;j++){
-				Wheel w = wheels.get(j+i*height);
+				Wheel w = wheels.get(j+i*width);
 				sb[0].append("       ");
 				sb[0].append(w.readNorth());
 				sb[2].append("       ");
@@ -266,12 +361,16 @@ public class Puzzle {
 		}
 		return globalsb.toString();
 	}
-	
+	/**
+	 * Returns a string which contains char of all wheels. The puzzle is read from left to right and from top to bottom
+	 * and each wheel is read in clockwise.
+	 * @return
+	 */
 	public String lineString() {
 		StringBuilder globalsb = new StringBuilder();
 		for(int j = 0;j<height;j++){
 			for(int i = 0;i<width;i++){
-				Wheel w = wheels.get(i+j*height);
+				Wheel w = wheels.get(i+j*width);
 				globalsb.append(w.readNorth());
 				globalsb.append(w.readEast());
 				globalsb.append(w.readSouth());
@@ -280,12 +379,18 @@ public class Puzzle {
 		}
 		return globalsb.toString();
 	}
-	
-	public static boolean isResolved(Puzzle p){
+	/**
+	 * Checks if a puzzle is solved. 
+	 * @param p the puzzle to check
+	 * @return true if p is solved, false otherwise
+	 */
+	public static boolean isSolved(Puzzle p){
+		if(p==null)
+			return false;
 		Wheel puzzle[][] = new Wheel[p.getWidth()][p.getHeight()];
 		for(int j = 0;j<p.getHeight();j++){
 			for(int i = 0;i<p.getWidth();i++){
-				puzzle[i][j] = p.getWheels().get(i+j*p.getHeight());
+				puzzle[i][j] = p.getWheels().get(i+j*p.getWidth());
 			}
 		}
 
@@ -307,11 +412,19 @@ public class Puzzle {
 		}
 		return true;
 	}
+	/**
+	 * Returns if the puzzle is equals to the given object. Two puzzle are equals if
+	 * they have the same width height and the contains the same wheels.
+	 * @param obj the given object
+	 * @return true if equals, false otherwise
+	 */
 	@Override
 	public boolean equals(Object obj) {
 		if (! (obj instanceof Puzzle) )
 			return false;
 		Puzzle p = (Puzzle) obj;
+		if(p.getHeight()!=this.getHeight() || p.getWidth()!=this.getWidth())
+			return false;
 		LinkedList<Wheel> res = new LinkedList<Wheel>();
 		List<Wheel> pWheel = p.getWheels();
 		for(Wheel pW : pWheel){
@@ -320,15 +433,19 @@ public class Puzzle {
 					res.add(pW);
 					break;
 				}
-
 			}
 		}
 		if(res.size()==wheels.size())
 			return true;
 		return false;
 	}
+	/**
+	 * Resolve the given puzzle. The puzzle HAVE to be resolvable.
+	 * If the puzzle is not resolvable an IllegalArgumentExecption will be throw
+	 * @param puzzle the puzzle to resolved
+	 */
 	public static void resolve(Puzzle puzzle){
-		if(isResolved(puzzle))
+		if(isSolved(puzzle))
 			return;
 		int width = puzzle.getWidth();
 		HashSet<Integer> usedWheelSet = new HashSet<Integer>();
