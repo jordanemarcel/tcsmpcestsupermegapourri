3703ab8e609d9b963e007784108f4d57a23e0fec
plouck <plouck@020f9b02-3690-11de-82fb-7f665ef830f7>
2009-05-18 17:17:44 +0000

    fucking !

--- src/fr/umlv/tcsmp/states/server/ApzlServerState.java
+++ src/fr/umlv/tcsmp/states/server/ApzlServerState.java
@@ -20,6 +20,9 @@ public class ApzlServerState extends TCSMPState {
 
 	private boolean send = false;
 	private boolean error = false;
+	private boolean relayed = false;
+	private boolean waitreply = false;
+	private boolean sendresponse = false;
 
 	// current response proccessed domain
 	private String currentDomain;
@@ -37,6 +40,46 @@ public class ApzlServerState extends TCSMPState {
 	@Override
 	public Response processCommand(Protocol proto, ByteBuffer bb) {
 
+		// relayed
+		if (relayed == true) {
+			relayed = false;
+			waitreply = true;
+			bb.clear();
+			return new Response(currentDomain, ResponseAction.READ);
+		}
+		
+		// wait reply from domain
+		if (waitreply == true) {
+			waitreply = false;
+			responses.addAll(Arrays.asList(TCSMPParser.slipResponseLine(TCSMPParser.decode(bb))));
+			try {
+				currentDomain = domains.get(0);
+			}
+			catch (Exception e) {
+				// last domain has been processed
+				TCSMPParser.multilinize(responses);
+				sendresponse = true;
+				bb.clear();
+				bb.put(TCSMPParser.encode(responses.remove(0)));
+				bb.flip();
+				if (responses.size() == 0) {
+					send = true;
+				}
+				return new Response(ResponseAction.WRITE);
+			}
+		}
+		
+		if (sendresponse == true) {
+			bb.clear();
+			bb.put(TCSMPParser.encode(responses.remove(0)));
+			bb.flip();
+			if (responses.size() == 0) {
+				sendresponse = false;
+				send = true;
+			}
+			return new Response(ResponseAction.WRITE);
+		}
+		
 		// response has been sent... OK.
 		if (send) {
 			if (error == false)
@@ -47,55 +90,55 @@ public class ApzlServerState extends TCSMPState {
 			return new Response(ResponseAction.READ);
 		}
 
-		// we wait for response from domains
-		if (domains != null || responses != null) {
-
-			// first call, we have to read response from the current domain
-			if (responses == null) {			
-				responses = new ArrayList<String>();
-				bb.clear();
-				return new Response(currentDomain, ResponseAction.READ);
-			}
-
-			// we have a response for the currentDomain
-			if (domains.size() != 0) {
-				// add its response
-				responses.addAll(Arrays.asList(TCSMPParser.slipResponseLine(TCSMPParser.decode(bb))));
-				domains.remove(currentDomain);
-				try {
-					currentDomain = domains.getFirst();
-					bb.clear();
-					return new Response(currentDomain, ResponseAction.READ);
-				} catch (Exception e) {
-					// seems there is no more domains, reply response to the
-					// client
-					TCSMPParser.multilinize(responses);
-					String res = responses.remove(0);
-					bb.clear();
-					bb.put(TCSMPParser.encode(res));
-					bb.flip();
-					if (responses.size() == 0) {
-						send = true;
-					}
-					return new Response(ResponseAction.WRITE);
-				}
-			}
-
-			// we are currently writing response to the client
-			if (responses.size() != 0) {
-				String res = responses.remove(0);
-				bb.clear();
-				bb.put(TCSMPParser.encode(res));
-				bb.flip();
-				// yeah we have finished
-				if (responses.size() == 0) {
-					send = true;
-				}
-				return new Response(ResponseAction.WRITE);
-			}
-
-			throw new AssertionError("response and domains list should not be empty");
-		}
+		//		// we wait for response from domains
+		//		if (domains != null || responses != null) {
+		//
+		//			// first call, we have to read response from the current domain
+		//			if (responses == null) {			
+		//				responses = new ArrayList<String>();
+		//				bb.clear();
+		//				return new Response(currentDomain, ResponseAction.READ);
+		//			}
+		//
+		//			// we have a response for the currentDomain
+		//			if (domains.size() != 0) {
+		//				// add its response
+		//				responses.addAll(Arrays.asList(TCSMPParser.slipResponseLine(TCSMPParser.decode(bb))));
+		//				domains.remove(currentDomain);
+		//				try {
+		//					currentDomain = domains.getFirst();
+		//					bb.clear();
+		//					return new Response(currentDomain, ResponseAction.READ);
+		//				} catch (Exception e) {
+		//					// seems there is no more domains, reply response to the
+		//					// client
+		//					TCSMPParser.multilinize(responses);
+		//					String res = responses.remove(0);
+		//					bb.clear();
+		//					bb.put(TCSMPParser.encode(res));
+		//					bb.flip();
+		//					if (responses.size() == 0) {
+		//						send = true;
+		//					}
+		//					return new Response(ResponseAction.WRITE);
+		//				}
+		//			}
+		//
+		//			// we are currently writing response to the client
+		//			if (responses.size() != 0) {
+		//				String res = responses.remove(0);
+		//				bb.clear();
+		//				bb.put(TCSMPParser.encode(res));
+		//				bb.flip();
+		//				// yeah we have finished
+		//				if (responses.size() == 0) {
+		//					send = true;
+		//				}
+		//				return new Response(ResponseAction.WRITE);
+		//			}
+		//
+		//			throw new AssertionError("response and domains list should not be empty");
+		//		}
 
 		// are we in timeout
 		if (isTimeout())
@@ -113,7 +156,7 @@ public class ApzlServerState extends TCSMPState {
 			error = true;
 			return new Response(ResponseAction.WRITE);
 		}
-		
+
 		if (args[0].equals("QUIT")) {
 			TCSMPState t = new QuitServerState();
 			proto.setState(t);
@@ -141,37 +184,38 @@ public class ApzlServerState extends TCSMPState {
 			return new Response(ResponseAction.WRITE);
 		}
 
-		// create a list with all the domain of the rctp
-		domains = new LinkedList<String>();
-		for (String r : proto.getRecpts()) {
-			try {
-				// XXX BOOOOOOOOOOO why do you do this to meh. Need only one appearance 
-				// of a domain even if two RCPTs have the same!
-				String domain = TCSMPParser.parseDomain(r); 
-				if (!domains.contains(domain))
-					domains.add(domain);
-			} catch (ParseException e) {
+		if (domains == null) {
+			// create a list with all the domain of the rctp
+			domains = new LinkedList<String>();
+			for (String r : proto.getRecpts()) {
+				try {
+					// XXX BOOOOOOOOOOO why do you do this to meh. Need only one appearance 
+					// of a domain even if two RCPTs have the same!
+					String domain = TCSMPParser.parseDomain(r); 
+					if (!domains.contains(domain))
+						domains.add(domain);
+				} catch (ParseException e) {
+				}
 			}
+			responses = new ArrayList<String>();
 		}
 
-		// set the current processed domain
-		currentDomain = domains.getFirst();
-
-		// reset cmd
-//		bb.position(0);
+		currentDomain = domains.remove(0);
 		
 		bb.clear();
 		bb.put(TCSMPParser.encode("APZL\r\n"));
 		bb.flip();
-
+		relayed = true;
+		
+		
 		// send it to any RCPT
-		return new Response(ResponseAction.RELAYALL);
+		return new Response(currentDomain, ResponseAction.WRITE);
 	}
 
 
 	@Override
 	public Response cancel(Protocol proto, ByteBuffer bb) {
-		
+
 		// we have received a cancel while we asking for PZL
 		// add a error message for this domain and switch
 		// to the next one
